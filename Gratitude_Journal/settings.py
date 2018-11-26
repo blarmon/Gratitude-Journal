c0263574d7c743e94460469a385539eeea1cc6f2
Siegel <csiegel@dmu.edu>
2018-11-26 12:17:32 -0600

    Set up my pstgres backend.

--- Gratitude_Journal/settings.py
+++ Gratitude_Journal/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Gratitude_Journal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gratitude_journal',
+        'USER': 'django',
+        'PASSWORD': 'djangoPass',
+        'HOST': '',
+        'PORT': '5432'
     }
 }
 
