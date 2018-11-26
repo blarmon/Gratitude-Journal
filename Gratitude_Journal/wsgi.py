34e8937a82bebda31431e0829ad024f344b5bdb1
Siegel <csiegel@dmu.edu>
2018-11-26 11:59:04 -0600

    Initialized project.

---
+++ Gratitude_Journal/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Gratitude_Journal project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Gratitude_Journal.settings')
+
+application = get_wsgi_application()
