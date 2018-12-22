969931bba29c3773be2e5ea4f4f8e78599afe9fc
Siegel <csiegel@dmu.edu>
2018-12-22 16:43:48 -0600

    changed settings

--- Gratitude_Journal/settings.py
+++ Gratitude_Journal/settings.py
@@ -125,7 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 MEDIA_URL = '/media/'
 
